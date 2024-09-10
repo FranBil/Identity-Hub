411b7d48db323656c8dca034e4822dce36675cfa
francis billa <francis.billa.fb@gmail.com>
2024-09-11 00:48:44 +0200

    IHUB-2: Set up deploy file

--- stacks/PersonStack.ts
+++ stacks/PersonStack.ts
@@ -19,26 +19,25 @@ export function PersonApiStack({ stack }: StackContext) {
 
     const listPersonFunction = new Function(stack, "GetAllPersons", {
         handler: "packages/lambda/list-persons/main.go",
+        description: "Function for list Persons",
         runtime: "go1.x",
         environment: {
             TABLE_NAME: "PersonTable"
         },
+        bind: [personsTable]
     });
 
     const createPersonFunction = new Function(stack, "CreatePerson", {
         handler: "packages/lambda/create-person/main.go",
+        description: "Function for create Person",
         runtime: "go1.x",
         environment: {
             TABLE_NAME: "PersonTable"
         },
+        bind: [personsTable]
     });
 
     const api = new Api(stack, "Api", {
-        defaults: {
-            function: {
-                bind: [personsTable]
-            },
-        },
         routes: {
             "GET /v1/persons": listPersonFunction,
             "POST /v1/persons": createPersonFunction,
