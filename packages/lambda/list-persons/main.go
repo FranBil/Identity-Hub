74ff5fb7018dd4ed4657448942c4291841800131
francis billa <francis.billa.fb@gmail.com>
2024-09-11 09:01:28 +0200

    IHUB-4: Add Logger

--- packages/lambda/list-persons/main.go
+++ packages/lambda/list-persons/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"identity-hub/packages/dynamodb"
+	"github.com/rs/zerolog/log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -13,9 +14,11 @@ type response events.APIGatewayProxyResponse
 func handler(request events.APIGatewayV2HTTPRequest) (response, error) {
 	items, err := dynamodb.GetAllPersonsInfo()
 	if err != nil {
+		log.Error().Msg("Error Getting Persons: %s", err)
 		return response{StatusCode: 500}, err
 	}
 
+	log.Info().Msg("Successfully fetched Persons: %s", fmt.Sprint(items))
 	return response{
 		Body: fmt.Sprint(items),
 		StatusCode: 200,
